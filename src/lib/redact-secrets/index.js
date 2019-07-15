58d8a118d1694120b3181108307a61eaa04282b6
David Kassa <david.kassa@gmail.com>
2019-07-14 20:32:45 -0500

    kill console.logs

--- src/lib/redact-secrets/index.js
+++ src/lib/redact-secrets/index.js
@@ -10,14 +10,12 @@ module.exports = function(redacted) {
   };
 
   function map(obj) {
-    console.log("map: " + JSON.stringify(obj) + " end_map");
     return traverse(obj).map(function(val) {
       if (isSecret.key(this.key) || isSecret.value(val)) this.update(redacted);
     });
   }
 
   function forEach(obj) {
-    console.log("forEach: " + JSON.stringify(obj) + " end_forEach");
     traverse(obj).forEach(function(val) {
       if (isSecret.key(this.key) || isSecret.value(val)) this.update(redacted);
     });
